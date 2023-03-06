4566f58212938f6740ba6fbba14f1d0f0f3f3b46
berf2608 <frederic.bergeron2@usherbrooke.ca>
2023-03-06 00:15:28 -0500

    fonction d'API pour l'ajout de semenciers

--- routes/semenciers.js
+++ routes/semenciers.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const MongoClient = require("mongodb").MongoClient;
+const Semencier = require("../data/Semencier");
 
-
+//TODO à rafiner plus tard pour gérer des params de recherches
 router.get("/", async function(req, res) {
     const client = new MongoClient(process.env.MONGO_URI);
     const semenciersCol = client.db("souverain").collection("semenciers");
     const cursor = semenciersCol.find({});
     res.send( await cursor.toArray());
+});
+
+router.post("/", async function(req, res){
+    const sem = new Semencier(req.body.nom, req.body.siteweb, req.body.adresse, req.body.fondation);
+    const client = new MongoClient(process.env.MONGO_URI);
+    const semenciersCol = client.db("souverain").collection("semenciers");
+    await semenciersCol.insertOne(sem);
+    res.status(201);
+    res.send(sem);
 });
 
 module.exports = router;
