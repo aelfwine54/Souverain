f4cd8bd8fb496c721859e116f45a81133c339556
berf2608 <frederic.bergeron2@usherbrooke.ca>
2023-03-06 22:57:52 -0500

    introduction de swagger et application à l'api semenciers

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const createError = require("http-errors");
 const express = require("express");
 const bodyParser = require("body-parser");
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 const path = require("path");
 const ValidationError = require("express-validation").ValidationError;
 const logger = require("morgan");
@@ -10,8 +12,6 @@ const eta = require("eta");
 config.config();
 
 const htmlRouter = require("./routes/html");
-const usersRouter = require("./routes/users");
-const semenciersRouter = require("./routes/semenciers");
 
 const app = express();
 
@@ -26,18 +26,53 @@ app.use(bodyParser.json());
 app.use(logger("dev"));
 app.use(express.static(path.join(__dirname, "public")));
 
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Souverain API par Swagger",
+            version: "0.1.0",
+            description: "Vue Swagger de l'API de Souverain",
+            license: {
+                name: "MIT",
+                url: "https://spdx.org/licenses/MIT.html",
+            },
+            contact: {
+                name: "Frédéric Bergeron",
+                url: "https://fredericbergeron.net",
+                email: "info@email.com",
+            },
+        },
+        servers: [
+            {
+                url: "http://localhost:3000",
+            },],
+    },
+    apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJsdoc(options);
+app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs, { explorer: true })
+);
+
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
+
 app.use("/html", htmlRouter);
-app.use("/users", usersRouter);
-app.use("/semenciers", semenciersRouter);
+app.use("/api/semenciers", require("./routes/semenciers"));
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     next(createError(404));
 });
+
 
 app.use(function(err, req, res) {
     if (err instanceof ValidationError) {
@@ -63,5 +98,6 @@ app.use(function(err, req, res) {
     res.status(err.status || 500);
     res.render("error");
 });
+
 
 module.exports = app;
--- routes/semenciers.js
+++ routes/semenciers.js
@@ -3,6 +3,83 @@ const router = express.Router();
 const MongoClient = require("mongodb").MongoClient;
 const Semencier = require("../data/Semencier");
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Semencier:
+ *       type: object
+ *       required:
+ *         - nom
+ *       properties:
+ *         _id:
+ *           type: string
+ *           description: L'id auto-généré du semencier
+ *         nom:
+ *           type: string
+ *           description: Le nom du semencier
+ *         fondation:
+ *           type: int
+ *           description: L'année de fondation de la compagnie
+ *         site_web:
+ *           type : string
+ *           description : L'adresse du site web de la compagnie
+ *         courriel:
+ *           type : string
+ *           format : email
+ *           description : L'adresse courriel de la compagnie
+ *         telephone:
+ *           type : string
+ *           description : Le numéro de téléphone de la compagnie
+ *         telecopieur:
+ *           type : string
+ *           description : Le numéro de télécopieur de la compagnie
+*         nombre_semences:
+ *           type : integer
+ *           description : Le nombre de types de semences offertes par la compagnie
+ *         adresse_postale:
+ *           type : object
+ *           properties:
+ *             code_postal:
+ *               type : string
+*             numero:
+ *               type : string
+*             pays:
+ *               type : string
+*             province:
+ *               type : string
+*             rue:
+ *               type : string
+*             ville:
+ *               type : string
+ *       example:
+ *         id: d5fE_asz
+ *         nom: McKenzie
+ *         fondation: 1896
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Semenciers
+ *   description: L'API pour gérer les semenciers
+ * /api/semenciers:
+ *   get:
+ *     summary : Récupérer la liste des semenciers
+ *     tags: [Semenciers]
+ *     responses:
+ *       200:
+ *         description: Une liste de semenciers.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               $ref: '#/components/schemas/Semencier'
+ *       500:
+ *         description: Some server error
+ *
+ */
+
 //TODO à rafiner plus tard pour gérer des params de recherches
 router.get("/", async function(req, res) {
     const client = new MongoClient(process.env.MONGO_URI);
@@ -10,6 +87,32 @@ router.get("/", async function(req, res) {
     const cursor = semenciersCol.find({});
     res.send( await cursor.toArray());
 });
+
+/**
+ * @swagger
+ * tags:
+ *   name: Semenciers
+ *   description: L'API pour gérer les semenciers
+ * /api/semenciers:
+ *   post:
+ *     summary: Ajouter un nouveau semencier
+ *     tags: [Semenciers]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Semencier'
+ *     responses:
+ *       200:
+ *         description: Le semencier créé.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Semencier'
+ *       500:
+ *         description: Some server error
+ */
 
 router.post("/", async function(req, res){
     const sem = new Semencier(req.body.nom, req.body.siteweb, req.body.adresse, req.body.fondation);
