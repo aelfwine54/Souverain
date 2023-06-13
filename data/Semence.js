49201b7e75b37a0577f69799692eb43a79fb36e7
berf2608 <frederic.bergeron2@usherbrooke.ca>
2023-06-12 20:21:22 -0400

    Refactor du format des semences. On sépare le nom du reste des trucs, qui s'en vont dans la propriété infos

--- data/Semence.js
+++ data/Semence.js
@@ -1,8 +1,10 @@
 class Semence{
-    constructor(nom_francais, nom_latin, nom_semencier) {
+    constructor(nom_francais, nom_anglais, nom_latin, nom_semencier, infos) {
         this.nom_francais = nom_francais;
         this.nom_latin = nom_latin;
         this.nom_semencier = nom_semencier;
+        this.infos = infos;
+        this.nom_anglais = nom_anglais;
     }
 }
 
--- routes/semences.js
+++ routes/semences.js
@@ -27,100 +27,103 @@ const Semence = require("../data/Semence");
  *         nom_semencier:
  *           type: string
  *           description: Le nom du semencier ("clé étrangère")
- *         variete:
- *           type: string
- *           description: La varitété de la semence
- *         etiquettes:
- *           description: Les étiquettes associées
- *           type: array
- *           items:
- *             type: string
- *         type_contenant:
- *           type : string
- *           description : Le type de contenant recommandé
- *         temps_avant_recolte:
- *           type : number
- *           description : Le temps prévu entre la plantation et la récolte
- *         temps_avant_germination:
- *           type: array
- *           items:
- *             type: number
- *           description : L'intervalle de temps entre la plantation et la germination. [min, max]
- *         description:
- *           type : string
- *           description : Le texte sur le paquet de semence
- *         instructions:
- *           type : string
- *           description : Les instructions de plantation textuelles données sur l'emballage
- *         prix_vente:
- *           type : float
- *           description : Le prix de vente
- *         profondeur_semence:
- *           type : object
+ *         infos:
+ *           type: object
  *           properties:
- *             pouces:
+ *             variete:
  *               type: string
- *               description : La profondeur de la semence en pouce (généralement une fraction)
- *             mm:
- *               type : number
- *               description : La profondeur de la semence en millimètre
- *         distance_semences:
- *           type : object
- *           properties:
- *             pouces:
- *               type: string
- *               description : La distance entre les semences en pouce (généralement une fraction)
- *             mm:
- *               type : number
- *               description : La distance entre les semences en millimètre
- *         distance_plants:
- *           type : object
- *           properties:
- *             pouces:
- *               type: string
- *               description : La distance entre les plants en pouce (généralement une fraction)
- *             cm:
- *               type : number
- *               description : La distance entre les plants en centimètre
- *         distance_rangs:
- *           type : object
- *           properties:
- *             pouces:
- *               type: string
- *               description : La distance entre les rangs en pouce (généralement une fraction)
- *             cm:
- *               type : number
- *               description : La distance entre les rangs en centimètre
- *         hateur_plants:
- *           type : object
- *           properties:
- *             pouces:
- *               type: string
- *               description : La hauteur de la plante en pouces
- *             cm:
- *               type : number
- *               description : La hauteur de la plante en centimètre
- *         code_fabriquant:
- *           type : object
- *           properties:
- *             nom_code:
- *               type: string
- *               description : Le nom (ou type) du code
- *             code:
+ *               description: La varitété de la semence
+ *             etiquettes:
+ *               description: Les étiquettes associées
+ *               type: array
+ *               items:
+ *                 type: string
+ *             type_contenant:
  *               type : string
- *               description : Le code de produit
- *         moment_recolte:
- *           type : object
- *           properties:
- *             mois:
- *               type: string
- *               description : Le mois de la récolte
- *             annee:
+ *               description : Le type de contenant recommandé
+ *             temps_avant_recolte:
  *               type : number
- *               description : L'année de la récolte
- *             jour:
- *               type : number
- *               description : Le jour de la récolte
+ *               description : Le temps prévu entre la plantation et la récolte
+ *             temps_avant_germination:
+ *               type: array
+ *               items:
+ *                 type: number
+ *               description : L'intervalle de temps entre la plantation et la germination. [min, max]
+ *             description:
+ *               type : string
+ *               description : Le texte sur le paquet de semence
+ *             instructions:
+ *               type : string
+ *               description : Les instructions de plantation textuelles données sur l'emballage
+ *             prix_vente:
+ *               type : float
+ *               description : Le prix de vente
+ *             profondeur_semence:
+ *               type : object
+ *               properties:
+ *                 pouces:
+ *                   type: string
+ *                   description : La profondeur de la semence en pouce (généralement une fraction)
+ *                 mm:
+ *                   type : number
+ *                   description : La profondeur de la semence en millimètre
+ *             distance_semences:
+ *               type : object
+ *               properties:
+ *                 pouces:
+ *                   type: string
+ *                   description : La distance entre les semences en pouce (généralement une fraction)
+ *                 mm:
+ *                   type : number
+ *                   description : La distance entre les semences en millimètre
+ *             distance_plants:
+ *               type : object
+ *               properties:
+ *                 pouces:
+ *                   type: string
+ *                   description : La distance entre les plants en pouce (généralement une fraction)
+ *                 cm:
+ *                   type : number
+ *                   description : La distance entre les plants en centimètre
+ *             distance_rangs:
+ *               type : object
+ *               properties:
+ *                 pouces:
+ *                   type: string
+ *                   description : La distance entre les rangs en pouce (généralement une fraction)
+ *                 cm:
+ *                   type : number
+ *                   description : La distance entre les rangs en centimètre
+ *             hauteur_plants:
+ *               type : object
+ *               properties:
+ *                 pouces:
+ *                   type: string
+ *                   description : La hauteur de la plante en pouces
+ *                 cm:
+ *                   type : number
+ *                   description : La hauteur de la plante en centimètre
+ *             code_fabriquant:
+ *               type : object
+ *               properties:
+ *                 nom_code:
+ *                   type: string
+ *                   description : Le nom (ou type) du code
+ *                 code:
+ *                   type : string
+ *                   description : Le code de produit
+ *             moment_recolte:
+ *               type : object
+ *               properties:
+ *                 mois:
+ *                   type: string
+ *                   description : Le mois de la récolte
+ *                 annee:
+ *                   type : number
+ *                   description : L'année de la récolte
+ *                 jour:
+ *                   type : number
+ *                   description : Le jour de la récolte
  */
 
 /**
@@ -169,7 +172,7 @@ router.get("/", async function(req, res) {
  *           schema:
  *             $ref: '#/components/schemas/Semence'
  *     responses:
- *       200:
+ *       201:
  *         description: La semence créée.
  *         content:
  *           application/json:
@@ -179,8 +182,9 @@ router.get("/", async function(req, res) {
  *         description: Some server error
  */
 
+// TODO Donner un warning si le semenciers n'existe pas
 router.post("/", async function(req, res){
-    const sem = new Semence(req.body.nom_francais, req.body.nom_latin, req.body.nom_semencier);
+    const sem = new Semence(req.body.nom_francais, req.body.nom_anglais, req.body.nom_latin, req.body.nom_semencier, req.body.infos);
     const client = new MongoClient(process.env.MONGO_URI);
     const semenciersCol = client.db("souverain").collection("semences");
     await semenciersCol.insertOne(sem);
